21d22f65e897fc992899a9e92929920bc12f365e
Marcel Alves <marcel.alves89@gmail.com>
2019-10-12 17:38:20 -0300

    feat: Implement the Presenter methods for loading the quiz
    The presenter updates the view when the quiz is retrieved from the web. The view may set the quiz title (question) or show an error message. The loading screen is dismissed after the information is retrieved by the interactor.

---
+++ QuizGame/Modules/Quiz/UserInterface/Presenter/QuizPresenter.swift
@@ -0,0 +1,61 @@
+//  Copyright 2019 ArcTouch LLC.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ------------------------------------------------------------------------
+//  ABSTRACT:
+//      QuizPresenter intermediates the communication between the passive view (QuizView) and the application logic (QuizInteractor).
+//      This class would also inform the Wireframe about the need to present a new screen, but this application does not need a Wireframe
+//      for now, since it has only one screen.
+
+protocol QuizPresenterInterface: AnyObject {
+    var view: QuizViewInterface? { get set }
+    var interactor: QuizInteractorInterface? { get set }
+    func viewDidLoad()
+}
+
+class QuizPresenter: QuizPresenterInterface {
+    
+    weak var view: QuizViewInterface?
+    var interactor: QuizInteractorInterface?
+    
+    func viewDidLoad() {
+        view?.startLoadingScreen()
+        // Currently, Java is the only possible Quiz.
+        // In the future, if more quizzes are supported, this needs to be changed.
+        interactor?.requestNewQuiz(withName: "Java")
+    }
+    
+}
+
+extension QuizPresenter: QuizInteractorDelegate {
+    
+    func retrievingQuizFailed(with error: QuizRequestError) {
+        view?.dismissLoadingScreen()
+        switch error {
+        case .invalidQuizName:
+            view?.showErrorMessage(title: "Invalid Quiz Name", text: "The quiz name is invalid. Try choosing another quiz.")
+        case .connectionError:
+            view?.showErrorMessage(title: "Connection Error", text: "There was a connection error. Check your internet connection.")
+        case .jsonParsingError:
+            // Since there's nothing that the user can do about a JSON parsing error,
+            // it's better to tell him/her to choose another quiz, if available.
+            view?.showErrorMessage(title: "Unavailable Quiz", text: "Right now this quiz is unavailable. Try another one.")
+        }
+    }
+    
+    func didRetrieveQuiz(quizQuestion: String, quizAnswer: [String]) {
+        view?.dismissLoadingScreen()
+        view?.setQuizTitle(quizQuestion)
+    }
+    
+}
---
+++ QuizGame/Modules/Quiz/UserInterface/View/QuizView.swift
@@ -0,0 +1,59 @@
+//  Copyright 2019 ArcTouch LLC.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ------------------------------------------------------------------------
+//  ABSTRACT:
+//      QuizView 
+
+import UIKit
+
+protocol QuizViewInterface: AnyObject {
+    var presenter: QuizPresenterInterface? { get set }
+    func startLoadingScreen()
+    func dismissLoadingScreen()
+    func setQuizTitle(_ title: String)
+    func showErrorMessage(title: String, text: String)
+}
+
+class QuizView: UIViewController, QuizViewInterface {
+    
+    var presenter: QuizPresenterInterface?
+    
+    func startLoadingScreen() {
+        
+    }
+    
+    func dismissLoadingScreen() {
+        
+    }
+    
+    func setQuizTitle(_ title: String) {
+        
+    }
+    
+    func showErrorMessage(title: String, text: String) {
+        
+    }
+    
+}
+
+// MARK: - Lifecycle
+
+extension QuizView {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.viewDidLoad()
+    }
+    
+}
