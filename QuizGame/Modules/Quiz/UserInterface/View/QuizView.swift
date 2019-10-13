0519b38ac7dd5b8cb7d2f604e804d356ba93c90e
Marcel Alves <marcel.alves89@gmail.com>
2019-10-13 12:17:28 -0300

    feat: Implement initial view configuration

--- QuizGame/Modules/Quiz/UserInterface/View/QuizView.swift
+++ QuizGame/Modules/Quiz/UserInterface/View/QuizView.swift
@@ -13,7 +13,18 @@
 //  limitations under the License.
 //  ------------------------------------------------------------------------
 //  ABSTRACT:
-//      QuizView 
+//      QuizView is a UIViewController subclass that contains all the UI of the Quiz Module.
+//
+//  NOTES:
+//      - It was decided to avoid the use of a UINavigationBar (or UINavigationController) because Apple discourages resizing
+//        UINavigationBar's height - see https://forums.developer.apple.com/thread/88202
+//        Highlights to this statement, from Apple Staff:
+//            "Finally the internal layout of the navigation bar is an implementation detail *including all class names and
+//             orderings*. Any code that relies upon the ordering of the navigation bar's subviews or the names of any of their
+//             classes is likely to encounter issues in the future, as these are all private details. Please do not rely upon
+//             them."
+//        Also, large title configuration seems to be suitable for one line titles, not multiline titles. All solutions for
+//        multiline titles are hacky and should face problems in the future.
 
 import UIKit
 
@@ -61,7 +72,36 @@ extension QuizView {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Call all initial view configurations
+        configureGuessTextField()
+        configureStartResetButton()
+        
+        // Inform the presenter that all the initial configuration is done
         presenter?.viewDidLoad()
+    }
+    
+    
+    
+}
+
+// MARK: - View Configuration
+
+extension QuizView {
+    
+    private func configureGuessTextField() {
+        // Remove border
+        guessTextField.setPaddingPoints(10)
+        guessTextField.borderStyle = .none
+        guessTextField.layer.masksToBounds = true
+        guessTextField.layer.cornerRadius = 5.0;
+        guessTextField.layer.backgroundColor = UIColor.white.cgColor
+        guessTextField.layer.borderColor = UIColor.clear.cgColor
+    }
+    
+    private func configureStartResetButton() {
+        startResetButton.layer.masksToBounds = true
+        startResetButton.layer.cornerRadius = 10.0
     }
     
 }
---
+++ QuizGame/Modules/Quiz/UserInterface/View/UIKit+Extensions.swift
@@ -0,0 +1,14 @@
+//  ABSTRACT:
+//      Useful extensions to some UIKit classes.
+
+import UIKit
+
+extension UITextField {
+    
+    func setPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+}
